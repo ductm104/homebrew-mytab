795487dffdc3f33b0e27f9f314b54090aa82537d
ductm104 <ductranminh104@gmail.com>
2026-08-14 11:54:27 +0700

    hanhcute v0.0.85

--- Casks/hanhcute.rb
+++ Casks/hanhcute.rb
@@ -1,6 +1,6 @@
 cask "hanhcute" do
-  version "0.0.84"
-  sha256 "fbbb4d7e06200fbe4a67b8d37192ba9faddad99522cca0854ca36adc0deaed5f"
+  version "0.0.85"
+  sha256 "5c330f75a13006f73d1c420bf196094fdf885009c6277ca1034806c8e43c1d3b"
 
   url "https://github.com/ductm104/Handy/releases/download/v#{version}/HanhCute_#{version}_aarch64.dmg",
       verified: "github.com/ductm104/Handy/"
